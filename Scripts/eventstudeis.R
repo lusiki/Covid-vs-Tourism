b0247731d7a95f42f93b23d3c952e55c7aa493fd
BrbanMiro <luka.sikic.gm@gmail.com>
2021-04-15 13:28:55 +0200

    weiter

--- Scripts/eventstudeis.R
+++ Scripts/eventstudeis.R
@@ -26,7 +26,14 @@ plot(es)
 
 
 
-es1 <- eventstudy(firm.returns = Returns,
+rates_indx <- get_rates_from_prices(Crobex, 
+                                    quote = "Close",
+                                    multi_day = TRUE,
+                                    compounding = "continuous")
+
+colnames(rates_indx) <- "Crobex"
+
+es2 <- eventstudy(firm.returns = Returns,
                  event.list = SplitD,
                  event.window = 40,
                  type = "marketModel",
@@ -36,7 +43,51 @@ es1 <- eventstudy(firm.returns = Returns,
                  inference.strategy = "bootstrap",
                  model.args = list(market.returns=rates_indx$Crobex))
 
-plot(es1)
+plot(es2)
+
+
+
+
+gold <- Quandl("LBMA/GOLD", type = "zoo", collapse = "daily", start_date = "2019-01-01",end_date = Sys.Date())
+gold <- gold[,5]
+
+
+add_indx <- get_rates_from_prices(gold, 
+                                  quote = "Close",
+                                  multi_day = TRUE,
+                                  compounding = "continuous")
+
+colnames(add_indx) <- "gold"
+
+es3 <- eventstudy(firm.returns = Returns,
+                  event.list = SplitD,
+                  event.window = 40,
+                  type = "marketModel",
+                  to.remap = TRUE,
+                  remap = "cumsum",
+                  inference = TRUE,
+                  inference.strategy = "bootstrap",
+                  model.args = list(market.returns=rates_indx$Crobex,
+                  others=add_indx$gold,
+                  market.returns.purge=TRUE))
+
+plot(es3)
+
+
+
+
+
+
+#VIDI 3rd paket
+#https://github.com/setzler/eventStudy
+
+
+
+
+
+
+
+
 
 
 
