be4622de3022c4b03f7cd3c803da8c6497a4d9c1
BrbanMiro <luka.sikic.gm@gmail.com>
2021-04-15 12:14:27 +0200

    los weiter

---
+++ Scripts/eventstudeis.R
@@ -0,0 +1,39 @@
+library(eventstudies)
+
+
+SplitD <- data.frame(Name=c(rep("ARNT", 40), 
+                            rep("MAIS", 40),
+                            rep("RIVP", 40)),
+                     Date = rep(seq(as.Date("2020-02-24"),
+                                    by = "day",
+                                    length.out = 40),3))
+
+SplitD <- data.frame(name=c("ARNT","MAIS","RIVP"),
+                     when = rep(as.Date("2020-02-24"),3))
+
+Returns <- na.omit(rates)
+
+es <- eventstudy(firm.returns = Returns,
+                 event.list = SplitD,
+                 event.window = 40,
+                 type = "None",
+                 to.remap = TRUE,
+                 remap = "cumsum",
+                 inference = TRUE,
+                 inference.strategy = "bootstrap")
+
+plot(es)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
